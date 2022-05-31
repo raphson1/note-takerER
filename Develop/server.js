134b0ff1921ddfe8a560bf9a014c0cc7737d86b2
raphson1 <raphsoneric@gmail.com>
2022-05-31 18:49:02 -0500

    server creation

---
+++ Develop/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const PORT = 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.listen(PORT, () =>{
+    console.log(`The server is listerning on ${PORT}`)
+});
