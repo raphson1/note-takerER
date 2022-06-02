7668b2ceba036390d19260f6b21de7ff61d08b04
raphson1 <raphsoneric@gmail.com>
2022-06-01 23:18:18 -0500

    deploye my app on heroku

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const notes = require('./db/db.json')
 const {v4: uuidv4} = require('uuid');
